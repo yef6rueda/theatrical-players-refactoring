e0f72f25ae4eb1f206775110a4680f7e834d07bc
Yef666 <yef6rueda@outlook.com>
2026-06-08 23:24:08 -0600

    refactor: split phase into statement data generation and text formatting

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -1,42 +1,58 @@
 package theatricalplays;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 public class StatementPrinter {
 
     public String print(Invoice invoice, Map<String, Play> plays) {
-        var result = String.format("Statement for %s%n", invoice.customer);
+        return renderPlainText(createStatementData(invoice, plays));
+    }
+
+    public StatementData createStatementData(Invoice invoice, Map<String, Play> plays) {
+        List<PerformanceData> performances = new ArrayList<>();
+        for (var perf : invoice.performances) {
+            var play = plays.get(perf.playID);
+            performances.add(new PerformanceData(
+                perf.playID,
+                play,
+                perf.audience,
+                calculateAmount(perf, play),
+                calculateVolumeCredits(perf, play)
+            ));
+        }
+        return new StatementData(invoice.customer, performances, getTotalAmount(performances), getTotalVolumeCredits(performances));
+    }
+
+    private String renderPlainText(StatementData data) {
+        var result = String.format("Statement for %s%n", data.customer());
 
         NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
-        for (var perf : invoice.performances) {
-            var play = plays.get(perf.playID);
-            var thisAmount = calculateAmount(perf, play);
-
+        for (var perf : data.performances()) {
             // print line for this order
-            result += String.format("  %s: %s (%s seats)%n", play.name, frmt.format(thisAmount / 100), perf.audience);
+            result += String.format("  %s: %s (%s seats)%n", perf.play().name, frmt.format(perf.amount() / 100), perf.audience());
         }
-        result += String.format("Amount owed is %s%n", frmt.format(getTotalAmount(invoice, plays) / 100));
-        result += String.format("You earned %s credits%n", getTotalVolumeCredits(invoice, plays));
+        result += String.format("Amount owed is %s%n", frmt.format(data.totalAmount() / 100));
+        result += String.format("You earned %s credits%n", data.totalVolumeCredits());
         return result;
     }
 
-    private int getTotalAmount(Invoice invoice, Map<String, Play> plays) {
+    private int getTotalAmount(List<PerformanceData> performances) {
         var result = 0;
-        for (var perf : invoice.performances) {
-            var play = plays.get(perf.playID);
-            result += calculateAmount(perf, play);
+        for (var perf : performances) {
+            result += perf.amount();
         }
         return result;
     }
 
-    private int getTotalVolumeCredits(Invoice invoice, Map<String, Play> plays) {
+    private int getTotalVolumeCredits(List<PerformanceData> performances) {
         var result = 0;
-        for (var perf : invoice.performances) {
-            var play = plays.get(perf.playID);
-            result += calculateVolumeCredits(perf, play);
+        for (var perf : performances) {
+            result += perf.volumeCredits();
         }
         return result;
     }
@@ -71,3 +87,6 @@ public class StatementPrinter {
     }
 
 }
+
+record StatementData(String customer, List<PerformanceData> performances, int totalAmount, int totalVolumeCredits) {}
+record PerformanceData(String playID, Play play, int audience, int amount, int volumeCredits) {}
