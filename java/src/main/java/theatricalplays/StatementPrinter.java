ae5a9e2a7aaad28ee66b31e7bf66af607930c5e2
Yef666 <yef6rueda@outlook.com>
2026-06-08 23:23:02 -0600

    refactor: extract performance amount calculation into calculateAmount

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -15,25 +15,7 @@ public class StatementPrinter {
 
         for (var perf : invoice.performances) {
             var play = plays.get(perf.playID);
-            var thisAmount = 0;
-
-            switch (play.type) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.audience > 30) {
-                        thisAmount += 1000 * (perf.audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (perf.audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.audience - 20);
-                    }
-                    thisAmount += 300 * perf.audience;
-                    break;
-                default:
-                    throw new Error("unknown type: %s".formatted(play.type));
-            }
+            var thisAmount = calculateAmount(perf, play);
 
             // add volume credits
             volumeCredits += calculateVolumeCredits(perf, play);
@@ -51,6 +33,28 @@ public class StatementPrinter {
         var result = 0;
         result += Math.max(perf.audience - 30, 0);
         if ("comedy".equals(play.type)) result += Math.floor(perf.audience / 5);
+        return result;
+    }
+
+    private int calculateAmount(Performance perf, Play play) {
+        var result = 0;
+        switch (play.type) {
+            case "tragedy":
+                result = 40000;
+                if (perf.audience > 30) {
+                    result += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (perf.audience > 20) {
+                    result += 10000 + 500 * (perf.audience - 20);
+                }
+                result += 300 * perf.audience;
+                break;
+            default:
+                throw new Error("unknown type: %s".formatted(play.type));
+        }
         return result;
     }
 
